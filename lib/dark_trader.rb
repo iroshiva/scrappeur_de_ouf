63f495974fbf4b487a13c81948789660764bf1e8
iroshiva <jean.weil@protonmail.com>
2019-07-17 23:17:17 +0200

    test dark_trader, array of curencies and one of prices....

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/dark_trader.rb'
+
 
 require 'nokogiri'
 require 'open-uri'
@@ -6,5 +6,30 @@ require 'pry'
 require 'rubocop'
 require 'rspec'
 
+
+
+
+page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+# puts page.xpath('/html/body/div[2]/div[2]/div[1]/div[1]/div[3]/div[2]/div/table/tbody/tr[1]/td[3]').text
+
+# puts table = page.xpath("//*[@id='currencies-all']").text 
+
+currencies = page.xpath('//td[3]').to_ary
+
+prices = page.xpath('//td[5]').to_ary
+
+
+currencies.each do |currency|
+	currency.text
+end
+
+
+prices.each do |price|
+	 puts price.text
+end
+
+# puts currencies.zip(prices).to_h
+
+# puts hash_simple = {currency[0] => prices[0]}
 
 
