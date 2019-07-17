466b19ae6609111661a29cd9391cc16db9a8438c
iroshiva <jean.weil@protonmail.com>
2019-07-18 00:09:07 +0200

    presque trader......

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -10,26 +10,71 @@ require 'rspec'
 
 
 page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
-# puts page.xpath('/html/body/div[2]/div[2]/div[1]/div[1]/div[3]/div[2]/div/table/tbody/tr[1]/td[3]').text
-
-# puts table = page.xpath("//*[@id='currencies-all']").text 
 
 currencies = page.xpath('//td[3]').to_ary
 
 prices = page.xpath('//td[5]').to_ary
 
+# currencies.each do |currency|
+# 	currency.text
+# end
 
-currencies.each do |currency|
-	currency.text
+# arr_curr_new = []
+# arr_curr_new << currencies
+
+
+def arr_curr(currencies)
+
+	arr_curr_new = []
+	currencies.each do |currency|
+		arr_curr_new <<	currency.text
+	end
+
+	arr_curr_new
+
 end
 
+# arr_curr(currencies)
 
-prices.each do |price|
-	 puts price.text
+
+
+def arr_pric(prices)
+
+	arr_pric_new = []
+	prices.each do |price|
+	 	arr_pric_new << price.text
+	end
+
+	arr_pric_new
+
 end
+
+# arr_pric(prices)
+
+
+# puts super_hash = Hash[arr_curr(currencies).zip(arr_pric(prices))]
+
+# p hash
 
 # puts currencies.zip(prices).to_h
 
-# puts hash_simple = {currency[0] => prices[0]}
+# puts hash_simple = {arr_curr(currencies)[0] => arr_pric(prices)[0]}
+
+i = 0
+super_array = []
+hash_simple = {}
+
+	2266.times do |i|
+		hash_simple = {arr_curr(currencies)[i] => arr_pric(prices)[i]}
+		i+=1
+
+	puts super_array << hash_simple	
+
+	end
+
+
+# puts super_array << hash_simple
+
+
 
 
