a654786b711343f34517e247302551ae7c6ae6d5
iroshiva <jean.weil@protonmail.com>
2019-07-17 19:13:57 +0200

    squelette du projet

---
+++ README.md
@@ -0,0 +1,1 @@
+Projet Scrappeur de ouf !!!!
---
+++ lib/dark_trader.rb
@@ -0,0 +1,10 @@
+require_relative '../lib/dark_trader.rb'
+
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+require 'rubocop'
+require 'rspec'
+
+
+
