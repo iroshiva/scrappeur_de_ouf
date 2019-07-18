b84d24cca4b2afae3046b6d0d5625086230cd306
iroshiva <jean.weil@protonmail.com>
2019-07-18 21:41:00 +0200

    ajout spec mairie et dark

--- spec/dark_trader_spec.rb
+++ spec/dark_trader_spec.rb
@@ -6,8 +6,10 @@ describe "the method which combine curr et price" do
   end
 
  # MARCHE PAS....
-  it "should return an array of hashes {currency => price" do
-    expect(perform).to eq({'BTC' => 9758.7})
+  it "should return an array of hashes including BTC, NEO, THR" do
+    expect(perform.reduce({}, :merge)).to include("BTC")
+    expect(perform.reduce({}, :merge)).to include("NEO")
+    expect(perform.reduce({}, :merge)).to include("THR")
   end
   
 end
--- spec/marie_cristmas_spec.rb
+++ spec/marie_cristmas_spec.rb
@@ -4,10 +4,4 @@ describe "the method which combine town et emails" do
   it "should return an array of hashes {town => emails not nil" do
     expect(perform).not_to be_nil
   end
-
- # MARCHE PAS....
-  # it "should return an array of hashes {currency => price" do
-  #   expect(perform).to eq({ 'BTC' => 9758.7})
-  # end
-  
 end
