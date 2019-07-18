cc846cc7ea1b9178128526739a4687d337be3f10
iroshiva <jean.weil@protonmail.com>
2019-07-18 10:42:13 +0200

    fin exo dark_trader.rb git add .git add .git add .git add . Youpiiiiigit add . reste dark_trader_spec.rb!

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -8,73 +8,85 @@ require 'rspec'
 
 
 
+def collect_lign_curr_price
+	# methode qui collecte un tableau contenant un tableau currencies et un tableau prices : [[currencies][prices]]
 
-page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+	page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+	currencies = page.xpath('//td[3]')
+	# met dans [currencies] toutes les lignes de code contenant chacune une currency
 
-currencies = page.xpath('//td[3]').to_ary
+	prices = page.xpath('//td[5]')
+	# met dans [prices] toutes les lignes de code contenant chacune un price
 
-prices = page.xpath('//td[5]').to_ary
-
-# currencies.each do |currency|
-# 	currency.text
-# end
-
-# arr_curr_new = []
-# arr_curr_new << currencies
-
-
-def arr_curr(currencies)
-
-	arr_curr_new = []
-	currencies.each do |currency|
-		arr_curr_new <<	currency.text
-	end
-
-	arr_curr_new
-
+	[currencies, prices]
 end
 
-# arr_curr(currencies)
+
+# METHODES INTERMÉDIAIRES QUE J'AI INTÉGRÉÉES DANS MA METHODE FINALE arr_comb_curr_price
+
+
+	# def arr_curr(currencies)
+
+	# 	arr_curr_new = []
+	# 	currencies.each do |currency|
+	# 		arr_curr_new <<	currency.text
+	# 	end
+
+	# 	arr_curr_new
+
+	# end
+
+	# puts arr_curr(currencies)
 
 
 
-def arr_pric(prices)
+	# def arr_pric(prices)
 
-	arr_pric_new = []
-	prices.each do |price|
-	 	arr_pric_new << price.text
-	end
+	# 	arr_pric_new = []
+	# 	prices.each do |price|
+	# 	 	arr_pric_new << price.text.strip[1..-1].to_f
+	# 	end
 
-	arr_pric_new
+	# 	arr_pric_new
 
+	# end
+
+	# # puts arr_pric(prices)
+
+
+def arr_comb_curr_price(array_curr, arr_price)
+	# methode qui va combiner [currencies] et [prices]
+	
+	i = 0
+	# variable
+
+	super_array = []
+	# création d'un nouvel array de hash
+
+	hash_simple = {}
+	# création d'un hash
+
+		array_curr.length.times do |i|
+		# permet de définir le nbr de répitition .times
+
+			hash_simple = {array_curr[i].text => arr_price[i].text.strip[1..-1].to_f}
+			# dans chaque hash, mettre en clé la currency.text (que le texte et pas les balises) et en valeur le price (idem .text, .strip enlève \n et $)
+
+		super_array << hash_simple
+		# met chaque hash généré dans un array
+
+		end
+
+		super_array
 end
 
-# arr_pric(prices)
 
+def perform
+	array_collect = collect_lign_curr_price
+	arr_comb_curr_price(array_collect[0], array_collect[1])
+end
 
-# puts super_hash = Hash[arr_curr(currencies).zip(arr_pric(prices))]
-
-# p hash
-
-# puts currencies.zip(prices).to_h
-
-# puts hash_simple = {arr_curr(currencies)[0] => arr_pric(prices)[0]}
-
-i = 0
-super_array = []
-hash_simple = {}
-
-	2266.times do |i|
-		hash_simple = {arr_curr(currencies)[i] => arr_pric(prices)[i]}
-		i+=1
-
-	puts super_array << hash_simple	
-
-	end
-
-
-# puts super_array << hash_simple
-
+puts perform
 
 
 
